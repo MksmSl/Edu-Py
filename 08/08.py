63794a8b74aef7566e60e05a62a3ebe78cc36787
MksmSl <maksym.slaston@gmail.com>
2023-07-02 17:01:25 +0100

    1

--- 08/08.py
+++ 08/08.py
@@ -10,14 +10,12 @@ def rounds_cats_in_hats(n_cats, rounds):
         print(cats)
         print(f'Total amount of iterations is {iterations}')
     else:
-        for i in range(n_cats + 1):  # list of cats wo hats
-            cats.append(False)
-            iterations += 1
+        cats = [False]*(n_cats+1)
         for i in range(1, rounds + 1):  # rounds
             iterations += 1
             for j in range(1, n_cats//i+1):  # putting on/off hats only to every numbers round cat
                 iterations += 1
-                cats[j*i] = False if cats[j*i] is True else True
+                cats[j*i] = not cats[j*i]
         print('List of cats ws a hat:')
         for i in range(1, n_cats + 1):  # printing numbers of cats ws a hat
             if cats[i] is True:
@@ -27,4 +25,4 @@ def rounds_cats_in_hats(n_cats, rounds):
 
 
 
-rounds_cats_in_hats(100, 100)
+rounds_cats_in_hats(100, 90)
