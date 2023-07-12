f9034d41b5841907a3160ab0aa746accdd609bff
MksmSl <maksym.slaston@gmail.com>
2023-07-12 18:17:03 +0100

    12

--- 12/12.py
+++ 12/12.py
@@ -1,49 +1,48 @@
-# def is_admin(func):
-#     def wrapper(**kwargs):
-#         user_type = kwargs['user_type'] 
-#         if user_type == 'admin':
-#             func(user_type)
-#         else:
-#             print('ValueError: Permission denied')
-#     return wrapper
+print('Task 1')
 
 
-# @is_admin
-# def show_customer_receipt(user_type: str):
-#     print('# Some very dangerous operation')
+def is_admin(func):
+    def wrapper(**kwargs):
+        user_type = kwargs['user_type'] 
+        if user_type == 'admin':
+            func(user_type)
+        else:
+            print('ValueError: Permission denied')
+    return wrapper
 
 
-# show_customer_receipt(user_type='admin')
+@is_admin
+def show_customer_receipt(user_type: str):
+    print('# Some very dangerous operation')
 
 
-# def duplicate_exec(func):
-#     def wrapper(*args):
-#         try:
-#             func(*args)
-#         except KeyError:
-#             print(f'Found 1 error during execution of your function: \
-# KeyError no such key as {next(iter(*args))}')
+show_customer_receipt(user_type='admin')
+
+
+print('Task 2')
+
+
+def duplicate_exec(func):
+    def wrapper(*args):
+        try:
+            func(*args)
+        except KeyError:
+            print(f'Found 1 error during execution of your function: \
+KeyError no such key as {next(iter(*args))}')
     
-#     return wrapper
+    return wrapper
 
 
-# @duplicate_exec
-# def some_function_with_risky_operation(data):
-#     print(data['key'])
+@duplicate_exec
+def some_function_with_risky_operation(data):
+    print(data['key'])
 
 
-# some_function_with_risky_operation({'keye': 'bar'})
+some_function_with_risky_operation({'keye': 'bar'})
 
 
-# @check_types
-# def add(a: int, b: int) -> int:
-#     return a + b
+print('Task 3')
 
-# add(1, 2)
-# > 3
-
-# add("1", "2")
-# > TypeError: Argument a must be int, not str
 
 def check_types(func):
     def wrapper(*args):
@@ -52,28 +51,37 @@ def check_types(func):
                 print(f'TypeError: Argument a must be int, not {type(i)}')
                 return print('Please, enter an int')
         if not isinstance(func(*args), int):
-            print(f'TypeError: Result a must be int, not {type(i)}')
+            print(f'TypeError: Result a must be int, not {type(func(*args))}')
         else:
             print(func(*args))
-
-                         
-                    
-
-        #     if isinstance(func(*args), int):
-        #         return func(*args)
-        #     else:
-        #         print(f'TypeError: Result a must be int, not {type(i)}')
-        # except Exception:
-        #     for i in args:
-        #         if not isinstance(i, int):
-        #             print(f'TypeError: Argument a must be int, not {type(i)}')
-        #         break
-        #     if not isinstance(func(*args), int):
-        #             print(f'TypeError: Result a must be int, not {type(i)}')
     return wrapper
+
 
 @check_types
 def add(a: int, b: int) -> int:
-    return a / b
+    return a + b
+
 
 add(8, 4)
+
+print('Task 4')
+
+
+cashe = {}
+def cash_decor(func):
+    def wrapper(arg):
+        if arg not in list(cashe.keys()):
+            cashe[arg] = func(arg)
+            print(func(arg))
+        else:
+            print(f'cashe result {cashe[4]}')
+    return wrapper
+
+
+@cash_decor
+def multiple_3(a):
+    return a**3
+
+
+multiple_3(4)
+multiple_3(4)
